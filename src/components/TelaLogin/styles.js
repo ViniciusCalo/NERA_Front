3e13987bab05faba38760497f241751fc4b81f59
thaisperlho <carvalho.thais0301@gmail.com>
2023-12-02 09:28:43 -0300

    update tela login aluno

--- src/components/TelaLogin/styles.js
+++ src/components/TelaLogin/styles.js
@@ -2,20 +2,15 @@ import styled from "styled-components";
 
 
 export const Box = styled.div`
-<<<<<<< HEAD
 margin-top: 10%;
 width: 1100px;
 height: 600px;
 flex-shrink: 0;
-=======
-margin: 10% 10% 10% 10%;
->>>>>>> dev-thais
 display: flex;
  justify-content: center;
   align-items: center;
   background: white;
   bottom: 0;
-<<<<<<< HEAD
   border-radius: 15px;
 `;
 
@@ -26,13 +21,6 @@ align-items: center;
 width: 100%;
 `
 
-=======
-  width: 80%;
-  height: 80%;
-  border-radius: 15px;
-`;
-
->>>>>>> dev-thais
 export const Logo = styled.img`
 margin: 5%;
 flex-shrink: 0;
@@ -44,16 +32,12 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-<<<<<<< HEAD
 margin-top: -10%;
 width: 60%;
 height: 450px;
 z-index: 0;
 border-radius: 15px;
 padding-top: 3%;
-=======
-width: 30%;
->>>>>>> dev-thais
 background-color: #F2B705;
 `
 export const vetor = styled.img`
@@ -61,17 +45,12 @@ margin: 5%;
 flex-shrink: 0;
 `
 export const Input = styled.input`
-<<<<<<< HEAD
 width: 80%;
-=======
-width: 60%;
->>>>>>> dev-thais
 height: 30px;
 padding: 2%;
 margin: 2%;
 border-radius: 15px;
 border: none;
-<<<<<<< HEAD
 font-size: 10px;
 `
 export const Button = styled.button`
@@ -79,13 +58,6 @@ width: 80%;
 height: 30px;
 margin: 2%;
 filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-=======
-`
-export const Button = styled.button`
-width: 60%;
-height: 30px;
-margin: 2%;
->>>>>>> dev-thais
 border: none;
 background-color: #034C8C;
 border-radius: 15px;
@@ -93,16 +65,10 @@ color: white;
 `
 
 export const ButtonG = styled.button`
-<<<<<<< HEAD
 width: 80%;
 height: 30px;
 margin: 2%;
 filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-=======
-width: 60%;
-height: 30px;
-margin: 2%;
->>>>>>> dev-thais
 border: none;
 background-color: white;
 border-radius: 15px;
@@ -113,11 +79,7 @@ display: flex;
 flex-direction: row;
 justify-content: right;
 align-items: center;
-<<<<<<< HEAD
 width: 70%;
-=======
-width: 80%;
->>>>>>> dev-thais
 `
 export const Text = styled.p`
 margin: 2%;
@@ -132,7 +94,6 @@ font-weight: 400;
 line-height: normal;
 text-decoration-line: underline;
 `
-<<<<<<< HEAD
 export const linha1 = styled.hr`
 position: absolute;
 width: 40%;
@@ -192,6 +153,4 @@ export const ButtonProf = styled.button`
   background-color: #F2B705;
   border-radius: 30px;
   `
-=======
->>>>>>> dev-thais
 
