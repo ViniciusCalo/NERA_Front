9aff11053fff06bb7d798878e1443b4d2852705f
thaisperlho <carvalho.thais0301@gmail.com>
2023-12-07 15:30:24 -0300

    create 6 files, creating screen cadastro

---
+++ src/components/TelaCadastro/styles.js
@@ -0,0 +1,204 @@
+import styled from "styled-components";
+
+
+export const Box = styled.div`
+margin-top: 10%;
+width: 1100px;
+height: 600px;
+flex-shrink: 0;
+display: flex;
+ justify-content: center;
+  align-items: center;
+  background: white;
+  bottom: 0;
+  border-radius: 15px;
+`;
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+`
+
+export const Div = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+width: 100%;
+`
+export const textEntrar = styled.p`
+color: #888585;
+font-family: Roboto;
+font-size: 20px;
+font-style: normal;
+font-weight: 300;
+line-height: normal;
+`
+
+export const ButtonEntrar = styled.a`
+display: flex;
+justify-content: center;
+align-items: center;
+text-decoration: none;
+width: 30%;
+height: 40px;
+margin-top: 5%;
+margin-bottom: 5%;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+border: none;
+background-color: #034C8C;
+border-radius: 15px;
+color: white;
+`
+
+export const Logo = styled.img`
+margin: 5%;
+flex-shrink: 0;
+width: 100%;
+`
+
+
+export const FormLogin = styled.form`
+position: relative;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 60%;
+height: 450px;
+border-radius: 15px;
+background-color: #F2B705;
+`
+export const Tituloform = styled.h1`
+color: #FFF;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+font-family: Roboto;
+font-size: 32px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+position: absolute;
+top: 10%;
+
+`
+export const vetor = styled.img`
+margin: 5%;
+flex-shrink: 0;
+`
+
+export const DivButton = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 85%;
+    `;
+
+export const InputS = styled.input`
+height: 30px;
+padding: 2%;
+margin: 2%;
+border-radius: 15px;
+border: none;
+font-size: 10px;
+`
+export const InputE = styled.input`
+width: 80%;
+height: 30px;
+padding: 2%;
+margin: 2%;
+border-radius: 15px;
+border: none;
+font-size: 10px;
+`
+
+
+export const Button = styled.button`
+width: 80%;
+height: 30px;
+margin-top: 5%;
+margin-bottom: 5%;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+border: none;
+background-color: #034C8C;
+border-radius: 15px;
+color: white;
+`
+
+export const ButtonG = styled.button`
+width: 80%;
+height: 30px;
+margin-top: 5%;
+margin-bottom: 5%;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+border: none;
+background-color: white;
+border-radius: 15px;
+color: black;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const icon = styled.img`
+margin-right: 10%;
+`
+export const div = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: right;
+align-items: center;
+width: 70%;
+`
+export const Text = styled.p`
+position: absolute;
+bottom: 10%;
+color: #000000B2;
+text-align: center;
+font-family: Roboto;
+font-size: 14px;
+font-style: normal;
+font-weight: 300;
+line-height: normal;
+`
+export const Link = styled.a`
+color: #000;
+font-family: Roboto;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+text-decoration-line: underline;
+`
+export const linha1 = styled.hr`
+position: absolute;
+width: 40%;
+height: 1px;
+margin: 2%;
+right: 0;
+border: none;
+background-color: black;
+border-radius: 15px;
+`
+export const linha2 = styled.hr`
+position: absolute;
+height: 1px;
+margin: 2%;
+width: 40%;
+border: none;
+background-color: black;
+border-radius: 15px;
+left: 0;
+`
+export const DivLinha = styled.div`
+position: relative;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+width: 60%;
+`;
+
+
+
+
---
+++ src/pages/Cadastro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalStyle from '../styles/login'
+import TelaCadastro from '../components/TelaCadastro'
+
+const Cadastro = () => {
+  return (
+    <>
+    <TelaCadastro/>
+    <GlobalStyle/>
+    </>
+  )
+}
+
+export default Cadastro
