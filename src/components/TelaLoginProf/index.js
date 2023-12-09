de4ca12255f03a5ed419cac0c7e5939b3f5dc97f
Kedsson Figueredo <kedsson19@gmail.com>
2023-12-08 22:38:41 -0300

    Login professor

--- src/components/TelaLoginProf/index.js
+++ src/components/TelaLoginProf/index.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import * as C from './styles'
 import Logo from './img/logo.png'
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 
 
 const TelaLoginProf = () => {
+  const nav = useNavigate()
+  const [sessao, setSesao] = useState([])
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+
+const handleEmail = (e) => {
+  setEmail(e.target.value)
+}
+
+const handleSenha = (e) => {
+  setSenha(e.target.value)
+}
+
+
+const handleVerificarUsuario = async () => {
+  var body = {
+    "email": email,
+    "senha": senha
+}
+try {
+    const response = await axios.post("http://localhost:443/usuario/login", body)
+    setSesao(response.data.message);
+    console.log(sessao)
+    localStorage.setItem('usuario', email);
+    nav('/perfil')
+} catch (err) {
+    console.log(err);
+    alert("senha e/ou email incorreto")
+}
+}
+
+
   return (
     <C.Box>
         <C.Logo src={Logo}/>
@@ -14,12 +50,12 @@ const TelaLoginProf = () => {
           <C.ButtonProf>Professor</C.ButtonProf>
         </C.DivButton>
         <C.FormLogin>
-        <C.Input type="text" placeholder="E-mail ou nome do usuário"/>
-        <C.Input type="password" placeholder="Senha"/>
+        <C.Input type="text" onChange={handleEmail} placeholder="E-mail ou nome do usuário"/>
+        <C.Input type="password" onChange={handleSenha} placeholder="Senha"/>
         <C.div>
             <C.Link>Esqueci minha senha?</C.Link>
         </C.div>
-        <C.Button>Entrar</C.Button>
+        <C.Button type='button' onClick={handleVerificarUsuario} >Entrar</C.Button>
         <C.DivLinha>
             <C.linha1></C.linha1>
             ou
